87d263e860a881587b2721404e571b793168b424
yama-choki <lovepingpong0@gmail.com>
2022-11-01 10:13:24 +0900

    Todo追加機能、フォーム検証機能追加、検証機能はRecuestフォルダに記載

---
+++ database/migrations/2022_10_31_153854_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 20);
+            $table->string('remarks', 50);
+            $table->boolean('status')->default(false);
+            $table->time('deadline');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->boolean('revision')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="flex flex-col min-h-[100vh]">
+    <header class="bg-slate-800">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="py-6">
+                <p class="text-white text-xl">Todoアプリ</p>
+            </div>
+        </div>
+    </header>
+
+    <main class="grow">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="py-[20px]">
+                <form action="/tasks" method="post" class="mt-10">
+                  @csrf
+
+                  <input type="text" name="title">
+                  <textarea name="remarks" id=""></textarea>
+                  <button type="submit">追加する</button>
+
+                </form>
+            </div>
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        </div>
+    </main>
+    <footer class="bg-slate-800">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6">
+        <div class="py-4 text-center">
+            <p class="text-white text-sm">Todoアプリ</p>
+        </div>
+    </div>
+    </footer>
+</body>
+
+</html>
